1002755b3d9fef02e515f00f2055af14fa5732cf
CachorroC <juankpato87@gmail.com>
2023-03-11 01:19:59 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
     Please enter the commit message for your changes. Lines starting
     with '' will be ignored, and an empty message aborts the commit.
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   .prettierrc
    	modified:   src/app/layout.tsx
    	modified:   src/components/navbar.tsx
    	modified:   src/styles/css/globals.css
    	modified:   src/styles/css/globals.css.map
    	modified:   src/styles/css/navbar.module.css
    	modified:   src/styles/css/navbar.module.css.map
    	modified:   src/styles/css/theme/colors.module.css
    	new file:   src/styles/css/theme/pruebas.css
    	new file:   src/styles/css/theme/theme.css
    	new file:   src/styles/css/theme/theme.css.map
    	modified:   src/styles/css/theme/theme.dark.css
    	modified:   src/styles/css/theme/theme.light.css
    	new file:   src/styles/css/theme/theme/tokens.css.map
    	modified:   src/styles/css/theme/tokens.css
    	modified:   src/styles/css/theme/tokens.css.map
    	modified:   src/styles/css/theme/variables.css
    	modified:   src/styles/css/theme/variables.css.map
    	modified:   src/styles/scss/globals.scss
    	modified:   src/styles/scss/navbar.module.scss
    	new file:   src/styles/scss/theme/_dark.scss
    	new file:   src/styles/scss/theme/_light.scss
    	modified:   src/styles/scss/theme/colors.module.css
    	renamed:    src/styles/scss/theme/_theme.scss -> src/styles/scss/theme/theme.css
    	modified:   src/styles/scss/theme/theme.dark.css
    	modified:   src/styles/scss/theme/theme.light.css
    	new file:   src/styles/scss/theme/tokens.css
    	deleted:    src/styles/scss/theme/tokens.scss
    	modified:   src/styles/scss/theme/variables.scss

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import styles from '#@/styles/css/navbar.module.css';
+import styles from '#@/styles/scss/navbar.module.scss';
 import { demos, type Item } from '#@/lib/demos';
 import { useState } from 'react';
 import Link from 'next/link';
@@ -9,102 +9,106 @@ import clsx from 'clsx';
 import { Poiret_One } from 'next/font/google';
 import 'material-symbols';
 
-const poiret = Poiret_One({
+const poiret = Poiret_One( {
   weight: '400',
-  subsets: ['latin', 'latin-ext'],
+  subsets: [ 'latin', 'latin-ext' ],
   display: 'swap',
-});
+} );
 
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const close = () => setIsOpen(false);
-  const drawerToggle = () => {
-    setIsOpen((prevState) => !prevState);
+const Navbar = () =>
+{
+  const [ isOpen, setIsOpen ] = useState( false );
+  const close = () => setIsOpen( false );
+  const drawerToggle = () =>
+  {
+    setIsOpen( ( prevState ) => !prevState );
   };
 
   const drawer = (
     <div
-      className={styles.drawer}
-      onClick={close}
+      className={ styles.drawer }
+      onClick={ close }
     >
-      {demos.map((section) => {
+      { demos.map( ( section ) =>
+      {
         return (
           <nav
-            key={section.name}
-            className={styles.menu}
+            key={ section.name }
+            className={ styles.menu }
           >
-            <h1 className={poiret.className}>{section.name}</h1>
-            <ul className={styles.list}>
-              {section.items.map((item) => (
+            <h1 className={ poiret.className }>{ section.name }</h1>
+            <ul className={ styles.list }>
+              { section.items.map( ( item ) => (
                 <NavItem
-                  key={item.slug}
-                  item={item}
-                  close={close}
-                  className={poiret.className}
+                  key={ item.slug }
+                  item={ item }
+                  close={ close }
+                  className={ poiret.className }
                 />
-              ))}
+              ) ) }
             </ul>
           </nav>
         );
-      })}
+      } ) }
     </div>
   );
   return (
     <>
-      <section className={styles.header}>
+      <section className={ styles.header }>
         <Link
           href="/"
-          onClick={close}
-          className={styles.button}
+          onClick={ close }
+          className={ styles.button }
         >
           <span className="material-symbols-outlined">cabin</span>
         </Link>
         <button
           type="button"
-          className={styles.button}
-          onClick={drawerToggle}
+          className={ styles.button }
+          onClick={ drawerToggle }
         >
-          {isOpen ? (
+          { isOpen ? (
             <span className="material-symbols-outlined">pets</span>
           ) : (
             <span className="material-symbols-outlined">star</span>
-          )}
+          ) }
         </button>
         <button
           type="button"
-          className={styles.button}
-          onClick={() => setIsOpen(!isOpen)}
+          className={ styles.button }
+          onClick={ () => setIsOpen( !isOpen ) }
         >
-          {isOpen ? (
+          { isOpen ? (
             <span className="material-symbols-outlined">close</span>
           ) : (
             <span className="material-symbols-outlined">menu</span>
-          )}
+          ) }
         </button>
       </section>
       <Drawer
         variant="temporary"
-        open={isOpen}
-        onClose={drawerToggle}
-        ModalProps={{ keepMounted: true }}
+        open={ isOpen }
+        onClose={ drawerToggle }
+        ModalProps={ { keepMounted: true } }
       >
-        {drawer}
+        { drawer }
       </Drawer>
     </>
   );
 };
 
-const NavItem = ({ item, className, close }: { item: Item; className: string; close: () => false | void }) => {
+const NavItem = ( { item, className, close }: { item: Item; className: string; close: () => false | void } ) =>
+{
   const segment = useSelectedLayoutSegment();
   const isActive = item.slug === segment;
   return (
     <Link
-      onClick={close}
-      href={`/${item.slug}`}
-      className={styles.item}
+      onClick={ close }
+      href={ `/${ item.slug }` }
+      className={ styles.item }
     >
-      <span className="material-symbols-outlined">{item.icon}</span>
-      <p className={className}>{item.name}</p>
+      <span className="material-symbols-outlined">{ item.icon }</span>
+      <p className={ className }>{ item.name }</p>
     </Link>
   );
 };
