d8154162c74238e32e8667849a088300d66cf6df
CachorroC <juankpato87@gmail.com>
2023-03-14 15:42:16 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
    DarkTheme Please enter the commit message for your changes. Lines starting
    DarkTheme with 'DarkTheme' will be ignored, and an empty message aborts the commit.
    DarkTheme
    DarkTheme On branch main
    DarkTheme Your branch is up to date with 'origin/main'.
    DarkTheme
    DarkTheme Changes to be committed:
    DarkTheme	modified:   src/app/page.tsx
    DarkTheme	modified:   src/lib/mongodb.ts
    DarkTheme	modified:   src/styles/css/globals.css
    DarkTheme	modified:   src/styles/css/globals.css.map
    DarkTheme	modified:   src/styles/css/navbar.module.css
    DarkTheme	new file:   src/styles/css/theme/darkTokens.css
    DarkTheme	new file:   src/styles/css/theme/darkTokens.css.map
    DarkTheme	new file:   src/styles/css/theme/lightTokens.css
    DarkTheme	new file:   src/styles/css/theme/lightTokens.css.map
    DarkTheme	modified:   src/styles/scss/globals.scss
    DarkTheme	new file:   src/styles/scss/theme/darkTokens.css
    DarkTheme	renamed:    src/styles/scss/theme/tokens.css -> src/styles/scss/theme/lightTokens.css
    DarkTheme	modified:   src/types/mongodb.d.ts
    DarkTheme	new file:   theme_test
    DarkTheme
    DarkTheme Changes not staged for commit:
    DarkTheme	modified:   theme_test (modified content)
    DarkTheme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,15 @@ import { Poiret_One } from 'next/font/google';
 import styles from '#@/styles/css/page.module.css';
 import Link from 'next/link';
 import { demos } from '#@/lib/links';
-import clientPromise from '#@/lib/mongodb';
-import { InferGetServerSidePropsType } from 'next';
 const poiret = Poiret_One({
   weight: '400',
   subsets: ['latin', 'latin-ext'],
   display: 'swap',
 });
 
-export async function getMongoData() {
-  try {
-    await clientPromise;
 
-    return {
-      props: {
-        isConnected: true,
-      },
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      props: {
-        isConnected: false,
-      },
-    };
-  }
-}
-export default async function Home({
-  isConnected,
-}: InferGetServerSidePropsType<typeof getMongoData>) {
+
+export default async function Home() {
   return (
     <div className={styles.grid}>
       {demos.map((section) => {
@@ -40,16 +20,7 @@ export default async function Home({
             key={section.name}
             className={styles.module}
           >
-            {isConnected ? (
-              <h2 className="subtitle">
-                You are connected to MongoDB
-              </h2>
-            ) : (
-              <h2 className="subtitle">
-                You are NOT connected to MongoDB. Check the{' '}
-                <code>README.md</code> for instructions.
-              </h2>
-            )}
+           
             <h2>{section.name}</h2>
             {section.items.map((item) => {
               return (
